9af1db12258a28bb73fad34395e44a81cd4c003b
CC <customcharacter@hotmail.com>
2026-08-26 17:27:03 -0600

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+
+namespace EO2Archi;
+
+[BepInPlugin("Etrian2.Archipelago", "Etrian Odyssey 2 Archipelago Plugin", "0.0.1")]
+public class Plugin : BasePlugin
+{
+    internal static new ManualLogSource Log;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log = base.Log;
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
---
+++ obj/Debug/net6.0/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace EO2Archi
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "EO2Archi";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
